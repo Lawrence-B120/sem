decea3558a4586f723f21b4d4d005ce275e2dd82
Joseph Mitchell <40442074@live.napier.ac.uk>
2020-02-16 18:26:29 +0000

    Added Population.java

---
+++ src/main/java/com/napier/sem/Population.java
@@ -0,0 +1,9 @@
+package com.napier.sem;
+
+//Class containing variables for Population details
+public class Population {
+    //Displayed Population details
+    public String name;
+    public int totalPop, cityPop, nonCityPop;
+    public float cityPercent, nonCityPercent;
+}
