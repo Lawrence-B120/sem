5a38a5e774fa8517b4be3903e233438497983503
Lawrence-B120 <lawrence-bennett@outlook.com>
2020-02-18 22:27:26 +0000

    PopCategory commit, Added constructor

--- src/main/java/com/napier/sem/PopulationCategories.java
+++ src/main/java/com/napier/sem/PopulationCategories.java
@@ -4,6 +4,12 @@ public class PopulationCategories {
     private String worldPop;
     private int areaTypeCount;
 
+    public PopulationCategories(String worldPop, int areaTypeCount)
+    {
+        this.worldPop = worldPop;
+        this.areaTypeCount = areaTypeCount;
+    }
+
     public String GetWorldPop()
     {
         return worldPop;
