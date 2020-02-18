f7b57f23c224178f46f86642f7b640e37e486078
Lawrence-B120 <lawrence-bennett@outlook.com>
2020-02-18 22:31:47 +0000

    PopCategory commit, Removed constructor as does not fit with current code method. Getters and setters suffice

--- src/main/java/com/napier/sem/PopulationCategories.java
+++ src/main/java/com/napier/sem/PopulationCategories.java
@@ -4,12 +4,6 @@ public class PopulationCategories {
     private String worldPop;
     private int areaTypeCount;
 
-    public PopulationCategories(String worldPop, int areaTypeCount)
-    {
-        this.worldPop = worldPop;
-        this.areaTypeCount = areaTypeCount;
-    }
-
     public String GetWorldPop()
     {
         return worldPop;
